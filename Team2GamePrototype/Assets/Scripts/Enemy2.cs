5c202f2280850e84449fdeaaaae26a6050e37c15
Adam Krenek <oof799444@gmail.com>
2025-10-23 09:56:45 -0500

    Added stuff that I worked on
    I added the enemy melee patrol script, placed the enemies around the map, and  edited some prefabs

--- Team2GamePrototype/Assets/Scripts/Enemy2.cs
+++ Team2GamePrototype/Assets/Scripts/Enemy2.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Enemy2 : MonoBehaviour
 {
-    public int health = 100;
+    public float maxHealth = 100f;
+    public float health = 100f;
 
-    public void TakeDamage(int damage)
+    private void Start()
+    {
+        health = maxHealth;
+        healthBar.UpdateHealthBar(health, maxHealth);
+    }
+    public void TakeDamage(float damage)
     {
         health -= damage;
+        healthBar.UpdateHealthBar(health, maxHealth);
 
         if (health <= 0)
         {
@@ -20,4 +27,9 @@ public class Enemy2 : MonoBehaviour
         Destroy(gameObject);
     }
 
+    [SerializeField] EnemyHealthbar healthBar;
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<EnemyHealthbar>();
+    }
 }
---
+++ Team2GamePrototype/Assets/Scripts/EnemyHealthbar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthbar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+
+    public void UpdateHealthBar(float currentValue, float maxValue)
+    {
+        slider.value = currentValue / maxValue;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
