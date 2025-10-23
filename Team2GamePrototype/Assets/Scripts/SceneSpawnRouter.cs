c025a581cc061eacb2d80f3771801a93e0979bc1
Adam Krenek <oof799444@gmail.com>
2025-10-23 15:20:58 -0500

    Removed broken project

--- Team2GamePrototype/Assets/Scripts/SceneSpawnRouter.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SceneSpawnRouter : MonoBehaviour
-{
-    // Set by the door you used; read by the spawn point in the next scene.
-    public static string NextSpawnId;
-}
