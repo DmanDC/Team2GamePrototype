02161629c695985c6f06b642d8ea319fa4cc638b
DmanDC <donovandch1@gmail.com>
2025-10-23 15:03:52 -0500

    Revert "Reapply "Merge branch 'main' of https://github.com/DmanDC/Team2GamePrototype""
    This reverts commit 11b3d96a97652f6812492f4d5a2cc74f6dba68be.

--- Team2GamePrototype/Assets/Scripts/Bullet2.cs
+++ Team2GamePrototype/Assets/Scripts/Bullet2.cs
@@ -7,9 +7,9 @@ public class Bullet2 : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
-    public float damage = 25f;
+    public int damage = 25;
     public GameObject player;
-
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +20,14 @@ public class Bullet2 : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
-        Enemy2 enemy = hitInfo.GetComponentInParent<Enemy2>();
-        if (enemy != null)
+        Enemy2 enemy = hitInfo.GetComponent<Enemy2>();
+        if (enemy != null && hitInfo.gameObject.tag == "Enemy")
         {
-            Debug.Log("Hit" + hitInfo.gameObject.name);
             enemy.TakeDamage(damage);
             Destroy(gameObject);
         }
 
-
+       
 
         // if bullet hits wall, destroy bullet
         if (hitInfo.gameObject.layer == LayerMask.NameToLayer("Ground"))
